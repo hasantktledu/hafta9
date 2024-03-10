e4fea00e1e79e7f4b790697a6345eeda0ee7bf16
Hasan Tokatlı Edu <hasantktl.edu@gmail.com>
2024-03-10 18:05:19 +0300

    Örnek kodlar yazıldı.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import { useEffect, useState } from "react";
 
 function App() {
+  const [sayac, sayacGuncelle] = useState(0)
+
+  useEffect( ()=>{ console.log("1. effect çalıştı") }, [sayac] )
+
+  useEffect( ()=>{ console.log("2. effect çalıştı")} )
+
+  
   return (
     <>
-      App
+     Sayaç: {sayac}
     </>
   );
 }
